b02f916cac30d5e3f28ea9bb418a293b53a50cd6
Sunday Ayandele <deaconsunday@gmail.com>
2024-07-04 10:22:58 +0200

    Update json2mdtable.py

--- json2mdtable.py
+++ json2mdtable.py
@@ -1,3 +1,50 @@
+trigger:
+- main
+
+pool:
+  vmImage: 'ubuntu-latest'
+
+steps:
+- task: UsePythonVersion@0
+  inputs:
+    versionSpec: '3.x'
+    addToPath: true
+
+- script: |
+    python -m pip install requests
+  displayName: 'Install dependencies'
+
+- script: |
+    python get_pipeline_details.py
+  displayName: 'Get Pipeline Details'
+  env:
+    SYSTEM_ACCESSTOKEN: $(System.AccessToken)
+
+- script: |
+    sudo apt-get update
+    sudo apt-get install -y jq
+    sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq
+    sudo chmod +x /usr/bin/yq
+  displayName: 'Install jq and yq'
+
+- script: |
+    jq -r '["Pipeline Name", "Repository Name", "YAML File Name", "Author", "Author Memberships"], (.[] | [.["Pipeline Name"], .["Repository Name"], .["YAML File Name"], .["Author"], (.["Author Memberships"] | join(", "))]) | @csv' pipeline_details.json > pipeline_details.csv
+
+    yq eval -P '. | 
+    "## Pipeline Details\n" + 
+    "| Pipeline Name | Repository Name | YAML File Name | Author | Author Memberships |\n| ------------- | --------------- | -------------- | ------ | ------------------ |\n" +
+    (.[] | "| " + .["Pipeline Name"] + " | " + .["Repository Name"] + " | " + .["YAML File Name"] + " | " + .["Author"] + " | " + (.["Author Memberships"] | join(", ")) + " |\n")' pipeline_details.json > pipeline_details.md
+  displayName: 'Convert JSON to CSV and Markdown'
+
+- publish: pipeline_details.csv
+  artifact: pipeline-details-csv
+  displayName: 'Publish Pipeline Details CSV'
+
+- publish: pipeline_details.md
+  artifact: pipeline-details-markdown
+  displayName: 'Publish Pipeline Details Markdown'
+
+========================================================
 import json
 import pandas as pd
 
